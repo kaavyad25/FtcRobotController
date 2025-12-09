daf079a328da53a7314ebb60a392a4b95686bb6d
Danush Ramanan <dan.ramanan@gmail.com>
2025-12-09 16:07:19 -0700

    find apriltags

---
+++ TeamCode/src/main/java/com/duck123acb/robotcore/Position.java
@@ -0,0 +1,12 @@
+package com.duck123acb.robotcore;
+
+public class Position {
+    public double x, y;
+    public double heading;
+
+    public Position(double x, double y, double heading) {
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallChaseOld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallChaseOld.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.duck123acb.robotcore.Position;
 import com.duck123acb.robotcore.Robot;
 import com.duck123acb.robotcore.RobotState;
+import com.duck123acb.robotcore.Position;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -13,7 +13,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * It initializes the real hardware and then passes control to the BallChaseLogic class.
  */
 @Autonomous(name = "BallChase_Decode")
-public class BallChase extends LinearOpMode {
+public class BallChaseOld extends LinearOpMode {
     static final int PURPLE = 1;
     static final int GREEN = 2;
     static final int[][] BALL_ORDERS = {{GREEN, PURPLE, GREEN}, {PURPLE, GREEN, PURPLE}, {PURPLE, PURPLE, GREEN}};
@@ -26,6 +26,50 @@ public class BallChase extends LinearOpMode {
 
     Robot robot;
     HuskyLens huskylens;
+
+//    @Override
+//    public void runOpMode() {
+//        // Initialize real hardware
+//        HuskyLens huskylens = hardwareMap.get(HuskyLens.class, "huskylens");
+//        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+//        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+//        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+//        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+//        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+//        DcMotor leftIntake = hardwareMap.get(DcMotor.class, "leftIntakeMotor");
+//        DcMotor rightIntake = hardwareMap.get(DcMotor.class, "rightIntakeMotor");
+//        DcMotor leftOuttake = hardwareMap.get(DcMotor.class, "leftOuttakeMotor");
+//        DcMotor rightOuttake = hardwareMap.get(DcMotor.class, "rightOuttakeMotor");
+//        Servo launch = hardwareMap.get(Servo.class, "launchServo");
+//
+//        // Set motor directions
+//        rightOuttake.setDirection(DcMotorSimple.Direction.REVERSE);
+//        leftFront.setDirection(DcMotor.Direction.FORWARD);
+//        leftBack.setDirection(DcMotor.Direction.FORWARD);
+//        rightFront.setDirection(DcMotor.Direction.REVERSE);
+//        rightBack.setDirection(DcMotor.Direction.REVERSE);
+//
+//        // Initialize IMU
+//        BNO055IMU.Parameters p = new BNO055IMU.Parameters();
+//        p.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+//        imu.initialize(p);
+//
+//        telemetry.addLine("Real hardware initialized");
+//        telemetry.update();
+//
+//        // Create the logic class and inject the hardware
+//        BallChaseLogic autonomousLogic = new BallChaseLogic(this, telemetry, huskylens, imu,
+//                leftFront, rightFront, leftBack, rightBack,
+//                leftIntake, rightIntake, leftOuttake, rightOuttake, launch);`
+//
+//        waitForStart();
+//
+//        if (opModeIsActive()) {
+//            autonomousLogic.run();
+//        }
+//    }
+
+    // TODO: reimplement apriltags
 
     @Override
     public void runOpMode() {
@@ -58,17 +102,47 @@ public class BallChase extends LinearOpMode {
         waitForStart();
         if (!opModeIsActive()) return;
 
-        // SET BALL ORDER
-        int[] ballOrder = BALL_ORDERS[0]; // set 0 as default
-        while (opModeIsActive()) {
-            int tag = SetAprilTag();
-            if (tag != -1) { // actually set it once found
-                ballOrder = BALL_ORDERS[tag];
-                break;
+        // choose pattern 0 for now
+        int[] pattern = BALL_ORDERS[0];
+
+        for (int targetColor : pattern) {
+            telemetry.addData("Next Target Ball", targetColor == PURPLE ? "PURPLE" : "GREEN");
+            telemetry.update();
+
+            // ---------------------------------------------------
+            // WAIT until we see the ball matching the next color
+            // ---------------------------------------------------
+            HuskyLens.Block targetBlock = getBlock(targetColor, huskylens);
+
+            if (targetBlock == null) break; // failsafe
+
+            // extract cam coords
+            int camX = targetBlock.x;
+            int camY = targetBlock.y;
+
+            double[] tgt = CamMath.cameraToField(targetBlock, robot);
+            double targetX = tgt[0];
+            double targetY = tgt[1];
+
+            RobotState s = robot.getState();
+            double dx = targetX - s.x;
+            double dy = targetY - s.y;
+            double targetHeading = Math.atan2(dy, dx);
+
+            turnTo(targetHeading);
+
+            telemetry.addData("Chasing Ball", "ID=%d cam(%d,%d)", targetColor, camX, camY);
+            telemetry.update();
+
+            while (opModeIsActive()) {
+                if (moveRobotToPosition(targetX, targetY, "Ball reached")) break;
             }
         }
 
+        telemetry.addLine("Finished full pattern");
+        telemetry.update();
 
+        goToBasketAndShoot();
     }
 
     private boolean moveRobotToPosition(double targetX, double targetY, String Ball_reached) {
@@ -206,8 +280,10 @@ public class BallChase extends LinearOpMode {
             );
             telemetry.update();
             return firstBlock.id;
-        }
-        else
+        } else {
+            telemetry.addData("No Tags Found", "");
+            telemetry.update();
             return -1;
+        }
     }
 }
